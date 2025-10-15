7c87986f6aa10c8e2bbda001924d1b506e5775f6
jatinpas <jatinpassi1422@gmail.com>
2025-10-16 00:30:53 +0530

    fixed

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { signIn } from "../redux/slices/authSlice"
 //  export const BASE_URL = "https://starfish-app-6yhui.ondigitalocean.app";
+// export const BASE_URL = "https://lionfish-app-mwu2u.ondigitalocean.app";
 export const BASE_URL = "https://lionfish-app-mwu2u.ondigitalocean.app";
 
 export const LoginWithOtp = async (mobile, setIsLoading) => {
