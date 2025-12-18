e77f52fa36bbb5dd4de7158e464acb13ab6ddab8
fastigo-ai <fastigo.in@gmail.com>
2025-12-18 14:21:12 +0530

    update2

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,6 +4,7 @@ import { signIn } from "../redux/slices/authSlice"
 // export const BASE_URL = "https://lionfish-app-mwu2u.ondigitalocean.app";
 // export const BASE_URL = "https://lionfish-app-mwu2u.ondigitalocean.app";
 export const BASE_URL = "https://king-prawn-app-5az7w.ondigitalocean.app";
+// export const BASE_URL = "http://localhost:3000";
 
 export const LoginWithOtp = async (mobile, setIsLoading) => {
     try {
@@ -104,4 +105,20 @@ export const fetchUserBookings = async () => {
   );
 
   return response.data.bookings;
+};
+
+export const cancelBooking = async ({ orderId, bookingId, propertyId }) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  };
+
+  const response = await axios.post(
+    `${BASE_URL}/api/payments/cancel-booking`,
+    { orderId, bookingId, propertyId },
+    config
+  );
+
+  return response.data;
 };
