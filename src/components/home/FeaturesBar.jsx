a5cd0d1fb503a09f79490cf714a2a01c76607ac5
fastigo-ai <fastigo.in@gmail.com>
2026-08-15 17:58:06 +0530

    final ui

--- src/components/home/FeaturesBar.jsx
+++ src/components/home/FeaturesBar.jsx
@@ -12,7 +12,7 @@ export default function FeaturesBar() {
   return (
     <div className="bg-[#D2B48C] border-b border-[#c2a47c]">
       <div className="max-w-7xl mx-auto px-6 py-6 overflow-x-auto hide-scrollbar">
-        {/* <div className="flex justify-between items-center min-w-max md:min-w-0 gap-8 md:gap-4">
+        <div className="flex justify-between items-center min-w-max md:min-w-0 gap-8 md:gap-4">
           {features.map((feature, index) => (
             <div key={index} className="flex items-center gap-3">
               <div className="w-10 h-10 rounded-full border border-[#c2a47c] flex items-center justify-center bg-white shadow-sm">
@@ -23,7 +23,7 @@ export default function FeaturesBar() {
               </span>
             </div>
           ))}
-        </div> */}
+        </div>
       </div>
     </div>
   );
--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -6,12 +6,13 @@ import RoomsSection from "../home/RoomsSection";
 import PromoBanner from "../home/PromoBanner";
 import WhyChooseUs from "../home/WhyChooseUs";
 import CTASection from "../home/CTASection";
+import FeaturesBar from "../home/FeaturesBar";
 
 const Home = () => {
   return (
     <div className="bg-[#ACA694]">
       <HeroSection />
-      
+      <FeaturesBar/>
       <WelcomeSection />
       <RoomsSection />
       <PromoBanner />
